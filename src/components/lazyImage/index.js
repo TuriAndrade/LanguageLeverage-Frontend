97c7ab659c7b8b9bb97a9e3820e7f7266758234d
Turi Vasconcelos <turivasconcelos@gmail.com>
2021-02-11 16:25:31 -0300

    Image alt flash bug fix

--- src/components/lazyImage/index.js
+++ src/components/lazyImage/index.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from "react"
 
 export default function LazyImage({ src, alt }) {
   const [isVisible, setIsVisible] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false)
 
   const observer = useRef()
   const lazyImage = useRef()
@@ -18,5 +19,15 @@ export default function LazyImage({ src, alt }) {
     if (lazyImage.current) observer.current.observe(lazyImage.current)
   }, [observer, lazyImage])
 
-  return <img ref={lazyImage} src={isVisible ? src : null} alt={alt}></img>
+  return isVisible ? (
+    <img
+      ref={lazyImage}
+      onLoad={() => setIsLoaded(true)}
+      src={src}
+      alt={alt}
+      style={!isLoaded ? { visibility: "hidden" } : { visibility: "visible" }}
+    ></img>
+  ) : (
+    <div ref={lazyImage}></div>
+  )
 }
